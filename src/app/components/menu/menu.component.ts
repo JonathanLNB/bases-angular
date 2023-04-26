004cc39510358ffbfc72c853e8ccd8f310c8f0f8
Jonathan Leonardo Nieto Bustamante <jonathanleonardonb@gmail.com>
2023-04-25 18:36:00 -0600

    [Update]

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+  rutas = [
+    {
+      name: 'Home',
+      path: '/home'
+    },
+    {
+      name: 'About',
+      path: '/about'
+    },
+    {
+      name: 'Contact',
+      path: '/contact'
+    }
+  ]
+}
